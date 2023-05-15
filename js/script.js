4eeee309c93310bfc6273b9385f7bd0be2edb636
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-15 12:12:27 +0300

    Slice arr from index 2 - 4

--- js/script.js
+++ js/script.js
@@ -11,5 +11,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 let arr = ['a', 'b', 'c', 'd', 'e', 'f'];
 
 const arr2 = arr.slice(2);
-console.log(newArr);
+console.log(arr2);
 
+const arr3 = arr.slice(2, 4);
+console.log(arr3);
