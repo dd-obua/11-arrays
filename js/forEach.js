a3459012d8ee4b2ef0ff71772f192920f38590c9
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-15 18:56:57 +0300

    Determine deposit or withdrawal using forEach

--- js/forEach.js
+++ js/forEach.js
@@ -6,3 +6,9 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 for (const movement of movements)
   if (movement > 0) console.log(`You deposited $${movement}.`);
   else console.log(`You withdrew $${Math.abs(movement)}.`);
+
+// Determine deposit and withdrawal using forEach method
+movements.forEach(movement => {
+  if (movement > 0) console.log(`You deposited $${movement}.`);
+  else console.log(`You withdrew $${Math.abs(movement)}`);
+});
