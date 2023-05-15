d6a15c5a0d323f7632d862e6a9814da227f38f42
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-15 18:59:42 +0300

    Sperate ouputs by logging empty space

--- js/forEach.js
+++ js/forEach.js
@@ -6,12 +6,14 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 for (const movement of movements)
   if (movement > 0) console.log(`You deposited $${movement}.`);
   else console.log(`You withdrew $${Math.abs(movement)}.`);
+console.log();
 
 // Determine deposit and withdrawal using forEach method
 movements.forEach(movement => {
   if (movement > 0) console.log(`You deposited $${movement}.`);
   else console.log(`You withdrew $${Math.abs(movement)}`);
 });
+console.log();
 
 // Refactor forEach
 movements.forEach(movement =>
