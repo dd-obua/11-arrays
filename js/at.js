739fad571e01d9c24c901e1b5ae66982475fa216
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-15 18:32:06 +0300

    Retrieve str charatacter at 10 using at method

--- js/at.js
+++ js/at.js
@@ -7,3 +7,7 @@ console.log(arr.at(1));
 console.log(arr[arr.length - 1]);
 console.log(arr.slice(-1)[0]);
 console.log(arr.at(-1));
+
+// Use at method on strings
+const str = 'Thank you Lord!';
+console.log(str.at(10));
