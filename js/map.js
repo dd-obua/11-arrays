9f803cda59f58df71c96921218fc8c28ae2e87d1
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-17 16:15:57 +0300

    Convert Euro to USD for each txn using map

--- js/map.js
+++ js/map.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const txns = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eruoToUsd = 1.1;
+
+const txnsUsd = txns.map(euro => euro * eruoToUsd);
+
+console.log(txns);
+console.log(txnsUsd);
