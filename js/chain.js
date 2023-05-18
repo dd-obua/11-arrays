c3c9629baf2b1cc35b4f998761c9e2ad85648101
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-18 15:40:58 +0300

    Rename totals valiable

--- js/chain.js
+++ js/chain.js
@@ -4,7 +4,7 @@ const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const euroToUsd = 1.1;
 // Take transation deposits, convert them from euros to dollars, then sum then up
-const totalDeposit = txnValues
+const totalDepositsUSD = txnValues
   .filter(val => val > 0)
   .map(val => val * euroToUsd)
   .reduce((acc, val) => acc + val, 0);
