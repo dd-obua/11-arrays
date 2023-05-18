bf35c9b1840c4009634330f35dd9f608222b1526
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-18 09:27:59 +0300

    Change val to txnVal

--- js/reduce.js
+++ js/reduce.js
@@ -2,5 +2,5 @@
 
 const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const balance = txnValues.reduce((acc, val) => acc + val);
+const balance = txnValues.reduce((acc, txnVal) => acc + txnVal);
 console.log('Balance:', balance);
