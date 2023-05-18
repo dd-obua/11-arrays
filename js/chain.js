6727784c4c8123e6af7c62f708a674d86db174f1
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-18 15:31:31 +0300

    Remove negative values

--- js/chain.js
+++ js/chain.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// Take transation deposits, convert them from euros to dollars, then sum then up
+
+const totalDeposit = txnValues.filter(txnVal => txnVal > 0);
