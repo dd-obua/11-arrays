fda40c089f6a9a938bea4dc2b9c04061c89ea94f
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-18 15:33:12 +0300

    Change txnVal to val

--- js/chain.js
+++ js/chain.js
@@ -4,4 +4,4 @@ const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // Take transation deposits, convert them from euros to dollars, then sum then up
 
-const totalDeposit = txnValues.filter(txnVal => txnVal > 0);
+const totalDeposit = txnValues.filter(val => val > 0);
