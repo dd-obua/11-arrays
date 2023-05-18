f60d987b4ebaa7b7eadb84ca041f218d85d8f9e8
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-18 12:42:13 +0300

    Calculate maximum value using reduce method

--- js/reduce.js
+++ js/reduce.js
@@ -4,3 +4,9 @@ const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const balance = txnValues.reduce((acc, txnVal) => acc + txnVal, 0);
 console.log('Balance:', balance);
+
+const max = txnValues.reduce(function (acc, txnVal) {
+  if (txnVal > acc) return txnVal;
+  else return acc;
+});
+console.log(max);
