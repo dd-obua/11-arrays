fb6aedcca0e70c9560d6c1f6ad6fea72ef9a54c7
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-18 12:44:07 +0300

    Factor max calculation using arrow function

--- js/reduce.js
+++ js/reduce.js
@@ -5,8 +5,9 @@ const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const balance = txnValues.reduce((acc, txnVal) => acc + txnVal, 0);
 console.log('Balance:', balance);
 
-const max = txnValues.reduce(function (acc, txnVal) {
-  if (txnVal > acc) return txnVal;
-  else return acc;
-});
+// const max = txnValues.reduce(function (acc, txnVal) {
+//   if (txnVal > acc) return txnVal;
+//   else return acc;
+// });
+const max = txnValues.reduce((acc, txnVal) => (acc > txnVal ? acc : txnVal));
 console.log(max);
