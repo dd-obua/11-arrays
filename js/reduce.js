5a17fcc57463e9e7c868fbecfd0a7233e3ba7f13
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-18 11:43:16 +0300

    Resolve merge conflict

--- js/reduce.js
+++ js/reduce.js
@@ -2,5 +2,5 @@
 
 const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const balance = txnValues.reduce((acc, txnVal, i) => acc + txnVal);
+const balance = txnValues.reduce((acc, txnVal, i) => acc + txnVal, 0);
 console.log('Balance:', balance);
