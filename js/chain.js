7d705216679e7cae8bfbe0b685c033f683125bd6
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-18 15:35:51 +0300

    Convert to USD

--- js/chain.js
+++ js/chain.js
@@ -4,4 +4,6 @@ const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const euroToUsd = 1.1;
 // Take transation deposits, convert them from euros to dollars, then sum then up
-const totalDeposit = txnValues.filter(val => val > 0);
+const totalDeposit = txnValues
+  .filter(val => val > 0)
+  .map(val => val * euroToUsd);
