0867e8e9a9353bad46cc371678eb65b123c14049
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 12:58:35 +0300

    Add accounts information

--- js/flat.js
+++ js/flat.js
@@ -11,3 +11,32 @@ console.log('arr2:', arr2);
 console.log('Deep flat arr2:', deepFlatArr2);
 
 console.log();
+
+// Add accounts information
+const account1 = {
+  owner: 'Abasa Wandega',
+  txns: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interstRate: 1.2, // 1.2%
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Simon Peter Ojok',
+  txns: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interstRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Wilber Natamba',
+  txns: [200, -200, 340, -300, -20, 50, 400, -460],
+  interstRate: 0.7,
+  pin: 333,
+};
+
+const account4 = {
+  owner: 'Pius Omoding',
+  txns: [430, 1000, 700, 50, 90],
+  interstRate: 1,
+  pin: 4444,
+};
