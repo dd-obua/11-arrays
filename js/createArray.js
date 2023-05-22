184824fe46ba090f014f040db2dbe01a8c80e464
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 16:18:15 +0300

    Define arr

--- js/createArray.js
+++ js/createArray.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // Create and fill arrays
-console.log([1, 2, 3, 4, 5, 6, 7]);
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
 console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
 // Create array programmatically
