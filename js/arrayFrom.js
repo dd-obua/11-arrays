f273606cd8dc023f5e718b50e0735c4f39af2da8
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 17:31:10 +0300

    Correct link to JavaScript file

--- js/arrayFrom.js
+++ js/arrayFrom.js
@@ -5,4 +5,5 @@ const selectAll = selector => document.querySelectorAll(selector);
 btn.addEventListener('click', function () {
   const items = selectAll('.item');
   const btn = select('.btn');
+  console.log('Clicked!');
 });
