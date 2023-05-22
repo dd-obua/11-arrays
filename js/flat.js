42be18a7a8df23df4cd19d0b2e5f1779cd44dfae
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 12:48:15 +0300

    Flatten an array with 2 levels of nesting

--- js/flat.js
+++ js/flat.js
@@ -4,3 +4,8 @@ const arr = [[1, 2, 3], 4, [5, 6], 7];
 const flatArr = arr.flat();
 console.log('arr:', arr);
 console.log('Flat arr:', flatArr);
+
+const arr2 = [[1, 2, [3, 4], 5], 6, 7, [8, [9, 10]]];
+const deepFlatArr2 = arr2.flat(2);
+console.log('arr2:', arr2);
+console.log('Deep flat arr2:', deepFlatArr2);
