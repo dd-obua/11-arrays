33d1ee3b77fc0fee71f6d7790ee41d0fce05c782
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 14:11:20 +0300

    Sort an array of numbers

--- js/sort.js
+++ js/sort.js
@@ -7,3 +7,13 @@ console.log('Owners:', owners);
 const sorted = owners.sort();
 console.log('Sorted owners:', sorted);
 console.log(owners);
+
+// Sort an array of numbers
+const nums = [200, 450, 450, -400, 3000, -650, -130, 70, 1300];
+console.log('nums:', nums);
+
+nums.sort((a, b) => {
+  if (a > b) return 1;
+  if (a < b) return -1;
+});
+console.log('Sorted nums:', nums);
