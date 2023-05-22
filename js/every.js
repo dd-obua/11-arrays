fef3f1f0b3ca959270bb393a1c78aff3638f443d
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 12:33:34 +0300

    Check if all transactions are deposits

--- js/every.js
+++ js/every.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const onlyDeposits = txnValues.every(txn => txn > 0);
+console.log('Only deposits?', onlyDeposits);
