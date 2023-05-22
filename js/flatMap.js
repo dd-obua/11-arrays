be6eee707431561a7b70e2d8f0f582d9893e0bea
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 13:06:08 +0300

    Add accounts information

--- js/flatMap.js
+++ js/flatMap.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// Add accounts information
+const account1 = {
+  owner: 'Abasa Wandega',
+  txns: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interstRate: 1.2, // 1.2%
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Simon Peter Ojok',
+  txns: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interstRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Wilber Natamba',
+  txns: [200, -200, 340, -300, -20, 50, 400, -460],
+  interstRate: 0.7,
+  pin: 333,
+};
+
+const account4 = {
+  owner: 'Pius Omoding',
+  txns: [430, 1000, 700, 50, 90],
+  interstRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
