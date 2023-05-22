b5f489f1ae0a72e81d3bb516189f7b4a83e795ac
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 14:11:02 +0300

    Sort an array of strings

--- js/sort.js
+++ js/sort.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const owners = ['Abasa', 'Wilber', 'Pous', 'Simon', 'Denis'];
+console.log('Owners:', owners);
+
+// Sort an array of strings
+const sorted = owners.sort();
+console.log('Sorted owners:', sorted);
+console.log(owners);
