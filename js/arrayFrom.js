213cb6bb57db0e7f2f1c1775beca905e39eb4696
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 17:04:56 +0300

    Select items and button

--- js/arrayFrom.js
+++ js/arrayFrom.js
@@ -2,3 +2,6 @@
 
 const select = sector => document.querySelector('selector');
 const selectAll = sector => document.querySelectorAll('selector');
+
+const items = selectAll('.item');
+const btn = select('.btn');
