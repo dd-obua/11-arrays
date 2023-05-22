c9a9affd6bda81278c5a65dcdc07487c2d217e72
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 16:14:07 +0300

    Create empty array

--- js/createArray.js
+++ js/createArray.js
@@ -4,3 +4,8 @@
 console.log([1, 2, 3, 4, 5, 6, 7]);
 console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
+// Create array programmatically
+// Using a constructor function
+const x = new Array(7);
+console.log(x); // creates an array with 7 empty items
+console.log(x.map(() => 5));
