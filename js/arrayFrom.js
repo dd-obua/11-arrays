a7ba0e4bfb86391446625876b5125f3db782aa1c
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 17:36:36 +0300

    Delete arrayFrom.js file

--- js/arrayFrom.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const select = selector => document.querySelector(selector);
-const selectAll = selector => document.querySelectorAll(selector);
-
-btn.addEventListener('click', function () {
-  const items = selectAll('.item');
-  const btn = select('.btn');
-  console.log('Clicked!');
-});
