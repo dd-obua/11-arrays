643f96b6bd6c5cd550d3a9417be8b4a95b1e2e80
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 19:33:38 +0300

    Refactor using chaining

--- js/arrayFrom.js
+++ js/arrayFrom.js
@@ -7,10 +7,8 @@ const items = selectAll('.item');
 const btn = select('.btn');
 
 btn.addEventListener('click', function () {
-  const txns = Array.from(items);
-  console.log(
-    txns
-      .map(el => Number(el.textContent.replace('$', '')))
-      .reduce((acc, num) => acc + num)
-  );
+  const sum = Array.from(items)
+    .map(el => Number(el.textContent.replace('$', '')))
+    .reduce((acc, num) => acc + num);
+  console.log('Sum:', sum);
 });
