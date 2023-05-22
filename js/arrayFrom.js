ea4d1669af913bfe7731f0f765663c20863f53d4
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 19:27:49 +0300

    Convet to an array of numbers

--- js/arrayFrom.js
+++ js/arrayFrom.js
@@ -8,5 +8,5 @@ const btn = select('.btn');
 
 btn.addEventListener('click', function () {
   const txns = Array.from(items);
-  console.log(txns.map(el => el.textContent.replace('$', '')));
+  console.log(txns.map(el => Number(el.textContent.replace('$', ''))));
 });
