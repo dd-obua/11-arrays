13b4aa8179b04eed9ebc35acbce7301d6052bcc9
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 11:00:10 +0300

    Check any condition

--- js/some.js
+++ js/some.js
@@ -4,3 +4,11 @@ const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // Check equality
 console.log('-130 present?', txnValues.includes(-130));
+
+// Check for any deposits
+// Check any condition
+const anyDeposits = txnValues.some(txn => txn > 0);
+console.log('Any deposits?', anyDeposits);
+
+const above5000 = txnValues.some(txn => txn > 5000);
+console.log('Any deposits above 5000?', above5000);
