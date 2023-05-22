c94da86e0777002063e0b96ca73850f1ac5c502f
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 13:02:14 +0300

    Flatten transactions into a single array

--- js/flat.js
+++ js/flat.js
@@ -44,5 +44,5 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Compute the total of all transactions
-const overallTxns = accounts.map(acct => acct.txns);
+const overallTxns = accounts.map(acct => acct.txns).flat();
 console.log(overallTxns);
