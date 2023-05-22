ea48892f23c00eae524c74b994c959a095c8e075
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 12:38:58 +0300

    Refactor deposit and withdrawal in seperate fns

--- js/every.js
+++ js/every.js
@@ -2,8 +2,11 @@
 
 const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const onlyDeposits = txnValues.every(txn => txn > 0);
+const deposit = txn => txn > 0;
+const withdrawal = txn => txn < 0;
+
+const onlyDeposits = txnValues.every(deposit);
 console.log('Only deposits?', onlyDeposits);
 
-const onlyWithdrawals = txnValues.every(txn => txn < 0);
+const onlyWithdrawals = txnValues.every(withdrawal);
 console.log('Only withdrawals?', onlyWithdrawals);
