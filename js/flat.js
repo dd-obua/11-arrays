facfda699baec30aedf766009ec43511cac9db98
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 12:58:00 +0300

    Create space

--- js/flat.js
+++ js/flat.js
@@ -9,3 +9,5 @@ const arr2 = [[1, 2, [3, 4], 5], 6, 7, [8, [9, 10]]];
 const deepFlatArr2 = arr2.flat(2);
 console.log('arr2:', arr2);
 console.log('Deep flat arr2:', deepFlatArr2);
+
+console.log();
