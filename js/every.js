e116c8e5acd144b3273c5c274b5cf5883faa8415
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 12:35:56 +0300

    Check if all transactions are withdrawals

--- js/every.js
+++ js/every.js
@@ -4,3 +4,6 @@ const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const onlyDeposits = txnValues.every(txn => txn > 0);
 console.log('Only deposits?', onlyDeposits);
+
+const onlyWithdrawals = txnValues.every(txn => txn < 0);
+console.log('Only withdrawals?', onlyWithdrawals);
