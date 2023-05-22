66379081cbc11542663413a2fad3b210194b1959
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 19:17:21 +0300

    Add $ to the figures

--- js/arrayFrom.js
+++ js/arrayFrom.js
@@ -5,3 +5,8 @@ const selectAll = selector => document.querySelectorAll(selector);
 
 const items = selectAll('.item');
 const btn = select('.btn');
+
+btn.addEventListener('click', function () {
+  const txns = Array.from(items);
+  console.log(txns);
+});
