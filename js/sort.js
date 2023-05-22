4a05ac20609fc6ec957db0cb9d3cc5415aa65944
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 14:21:26 +0300

    Refactor sort conditions

--- js/sort.js
+++ js/sort.js
@@ -13,15 +13,9 @@ const nums = [200, 450, 450, -400, 3000, -650, -130, 70, 1300];
 console.log('nums:', nums);
 
 // Ascending order
-nums.sort((a, b) => {
-  if (a > b) return 1;
-  if (a < b) return -1;
-});
+nums.sort((a, b) => a - b);
 console.log('Sorted nums (ascending):', nums);
 
 // Descending order
-nums.sort((a, b) => {
-  if (a > b) return -1;
-  if (b < b) return 1;
-});
+nums.sort((a, b) => b - a);
 console.log('Sorted nums (descending):', nums);
