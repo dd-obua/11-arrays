7113b6b62e539f7a4aad6877838092a630323fe9
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 19:26:34 +0300

    Remove $ from each figure

--- js/arrayFrom.js
+++ js/arrayFrom.js
@@ -8,5 +8,5 @@ const btn = select('.btn');
 
 btn.addEventListener('click', function () {
   const txns = Array.from(items);
-  console.log(txns);
+  console.log(txns.map(el => el.textContent.replace('$', '')));
 });
