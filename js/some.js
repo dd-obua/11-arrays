353fdeafc9b9184d6cc39950e5cf707b850488f5
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 10:59:42 +0300

    Check equality

--- js/some.js
+++ js/some.js
@@ -2,5 +2,5 @@
 
 const txnValues = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-console.log(txnValues.includes(-130));
-
+// Check equality
+console.log('-130 present?', txnValues.includes(-130));
